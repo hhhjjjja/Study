ad148776146fbbae0327ea36b1da0ef223757d5c
hhhjjjja <59678496+hhhjjjja@users.noreply.github.com>
2021-02-26 17:55:59 +0900

    2021.02.26 청소년 상어쓰 외않되...

--- JAVA/boj19236_청소년상어/src/boj19236_청소년상어/Main.java
+++ JAVA/boj19236_청소년상어/src/boj19236_청소년상어/Main.java
@@ -3,11 +3,6 @@ package boj19236_청소년상어;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -21,17 +16,16 @@ public class Main {
 		}
 	}
 	
-	static int[][] map = new int[4][4];
-	static Info[] fishes = new Info[17];
+	//1부터 ↑, ↖, ←, ↙, ↓, ↘, →, ↗
+	static int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};
+	static int[] dc = {0, -1, -1, -1, 0, 1, 1, 1};
 	
-	//[0]은 사용x, 1부터 ↑, ↖, ←, ↙, ↓, ↘, →, ↗
-	static int[] dr = {0, -1, -1, 0, 1, 1, 1, 0, -1};
-	static int[] dc = {0, 0, -1, -1, -1, 0, 1, 1, 1};
-	
-	static int answer = 0;
-	static int sr, sc, sd;			//상어 좌표 r, c, 상어방향 sd
+	static int max = 0;
 	
 	public static void main(String[] args) throws IOException {
+		int[][] map = new int[4][4];
+		Info[] fishes = new Info[17];
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
@@ -47,66 +41,102 @@ public class Main {
 			}
 		}
 		
-		sr = sc = 0;		//상어 위치 (0, 0)
-		sd = fishes[map[0][0]].direct;		//상어 방향 (0,0)에 있던 물고기의 방향
+		int answer = map[0][0];		//(0, 0) 물고기 먹었으니까 물고기 번호+
+		Info shark = new Info(0, 0, fishes[map[0][0]].direct);		//상어 시작위치는 0,0
 		
-		answer += map[0][0];		//(0, 0) 물고기 먹었으니까 물고기 번호+
-		fishes[map[0][0]] = null;	//먹었으니 지우기
+		fishes[0] = new Info(0, 0, -1);		//null칸 채워주기
+		
+		fishes[map[0][0]] = new Info(0, 0, -1);	//먹었으니 지우기
 		map[0][0] = -1;				//상어위치 state -1
 		
-		dfs();
+		dfs(map, fishes, answer, shark);
 		
-		System.out.println(answer);
+		System.out.println(max);
 	}
 	
-	static void dfs() {
+	static void dfs(int[][] map, Info[] fishes, int answer, Info shark) {
+		//원래 데이터 유지위해서 데이터 카피
+		int[][] map2 = new int [4][4];
+		Info[] fishes2 = new Info[17];
+		
+		for(int i=0; i<4; ++i) {
+			System.arraycopy(map[i], 0, map2[i], 0, 4);
+		}
+		
+		for(int i=1; i<17; ++i) {
+			fishes2[i] = new Info(fishes[i].r, fishes[i].c, fishes[i].direct);
+		}
+		
 		//물고기 이동 1번부터 16번까지
 		for(int i=1; i<=16; ++i) {
-			Info fish = fishes[i];		//지금 물고기
+			if(fishes2[i].direct == -1) continue;
 			
-			if(fish == null) continue;
+			Info fish = fishes2[i];		//지금 물고기
 			
 			int nd = fish.direct;
 			//이동할 수 있는 방향 찾기
-			while(true) {
-				boolean flag = false;
+			for(int d=0; d<8; ++d) {
+				nd %= 8;
 				
-				int nr = fish.r + dr[fish.direct];
-				int nc = fish.c + dc[fish.direct];
+				int nr = fish.r + dr[nd];
+				int nc = fish.c + dc[nd];
 				
 				//맵에서 벗어나거나 상어가 있으면 반시계 45도 회전
 				if(nr<0 || nr>=4 || nc<0 || nc>=4 || map[nr][nc] == -1) {
-					fish.direct = (fish.direct + 1) % 8;
-					flag = true;
+					nd++;
 					continue;
 				}
-				
-				Info t = fishes[map[nr][nc]];
-				//물고기 없는칸으로 이동하면 지금 물고기 정보만 갱신
-				if(t == null) {
-					fishes[i] = new Info(nr, nc, fish.direct);
-				} else {
+		
+				if(map2[nr][nc] == 0) {
+					map2[fish.r][fish.c] = 0;
+					map2[nr][nc] = i;
+					fishes2[i] = new Info(nr, nc, nd);
+					break;
+				} else if(map2[nr][nc] > 0 ) {
+					map2[fish.r][fish.c] = map2[nr][nc];
+					
 					//빈칸이 아니면 물고기 정보 서로 교환
-					fishes[map[nr][nc]] = new Info(fish.r, fish.c, t.direct);	//이동할 좌표에 지금물고기 정보로 갱신
-					fishes[i] = new Info(nr, nc, fish.direct);		//지금물고기 있던 칸으로 이동한좌표에 있던 물고기 정보 갱신
+					fishes2[map2[nr][nc]] = new Info(fish.r, fish.c, fishes2[map2[nr][nc]].direct);	//이동할 좌표에 지금물고기 정보로 갱신
+					map2[nr][nc] = i;
+					fishes2[i] = new Info(nr, nc, nd);		//지금물고기 있던 칸으로 이동한좌표에 있던 물고기 정보 갱신
+					break;
 				}
-				
-				int mtmp = map[nr][nc];
-				map[nr][nc] = i;
-				map[fish.r][fish.c] = mtmp;
-				
-				//돌아간 적이 있고 시작했던 방향(nd) == 마지막 방향(fish.direct)이면 이동할수있는 칸이 없음
-				if(flag && nd == fish.direct) break;
 			}
 		}
 		
+		boolean ff = false;
+		
 		//상어 이동
-		for(int i=1; i<4; ++i) {
-			int nr = sr + dr[sd]*i;
-			int nc = sc + dr[sd]*i;
+		for(int i=1; i<=4; ++i) {
 			
-			if(nr<0 || nr>=4 || nc<0 || nc>=3) break;
-			if(map[nr][nc] == 0) continue;
+			int nr = shark.r + dr[shark.direct]*i;
+			int nc = shark.c + dc[shark.direct]*i;
+			
+			//맵에서 벗어나면 break
+			if(nr<0 || nr>=4 || nc<0 || nc>=4) break;
+			
+			int tmp = map2[nr][nc];
+			
+			//물고기 있는 칸이면
+			if(tmp > 0) {
+				ff = true;
+				
+				map2[shark.r][shark.c] = 0;
+				
+				//상어 이동, 정보갱신
+				Info s = new Info(nr, nc, fishes2[tmp].direct);
+				//(nr, nc)좌표에 있던 물고기 먹었으니까 물고기 지우기
+				fishes2[tmp] = new Info(0, 0, -1);
+				map2[nr][nc] = -1;
+				
+				dfs(map2, fishes2, answer+tmp, s);
+				
+				map2[nr][nc] = tmp;		//값 되돌려놓기
+				fishes[tmp] = new Info(nr, nc, s.direct);
+				map2[shark.r][shark.c] = -1;
+			}
 		}
+		
+		if(!ff) max = Math.max(max, answer);
 	}
 }
